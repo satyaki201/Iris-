5f87c9aeb1619a075a6eda2cff5a90e2dc602028
SATYAKI SAHA <satyaki01saha@gmail.com>
2024-07-17 11:31:37 +0000

    ok

---
+++ demo/src/main/java/com/example/demo/Enumeration.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class Enumeration {
+    public static void main(String[] args) {
+        //Options in shop SMALL,MEDIUM,LARGE
+        //when we describe a size of pizza we can say small SMALL Small or by mistake smalll or smmall
+        //a human it is understandable but not a computer and we want our computer to understand that too.
+        // actually we can store 3 data with just 2 bits but if we use such large notions mistakes and storage is wasted hence comes enumerator
+        //character in java is unicode and we can store anything in this character may it be chinese or hindi hece is 16 bits
+
+        
+    }
+}
